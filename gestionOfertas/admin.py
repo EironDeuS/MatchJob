6913fb878ad4904bf67e327237ccbece20c8def3
Cristian Ruiz <164034589+caezo@users.noreply.github.com>
2025-04-10 21:53:05 -0400

    Update admin.py

--- gestionOfertas/admin.py
+++ gestionOfertas/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 
-# Register your models here.gaeregadgfdgdfagdfgdafgdf fdsfadsfsadfdsafads
+# Register your models here.gaeregadgfdgdfagdfgdafgdf fdsfadsfsadfdsafads   
 
